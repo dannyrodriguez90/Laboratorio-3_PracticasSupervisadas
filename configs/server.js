6289de9356d2cce675bc056b17caffbb58fb34a9
dannyrodriguez90 <rmdanny889@gmail.com>
2025-02-17 09:04:40 -0600

    fuciones de registro y login implementados

--- configs/server.js
+++ configs/server.js
@@ -5,6 +5,7 @@ import cors from "cors"
 import helmet from "helmet"
 import morgan from "morgan"
 import { dbConnection } from "./mongo.js"
+import authRoutes from "../src/auth/auth.routes.js";
 import apiLimiter from "../src/middlewares/rate-limit-validator.js"
 
 
@@ -28,8 +29,7 @@ const conectarDB = async () =>{
 }
 
 const routes = (app) => {
-    /*app.use("/proyectoBimestral/v1/auth", authRoutes);
-    app.use("/proyectoBimestral/v1/usuario", usuarioRoutes);*/
+    app.use("/uniNet/v1/auth", authRoutes);
 }
 
 export const initServer = () => {
